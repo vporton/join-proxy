20b16107a9eaf2fe333e8da67880d73746dc88cb
Victor Porton <porton.victor@gmail.com>
2024-05-08 03:02:49 +0300

    rewritten, now will test

--- src/app.py
+++ src/app.py
@@ -1,33 +1,82 @@
-import re
-import struct
+import copy
+import hashlib
+import itertools
+import operator
+import time
+from multidict import CIMultiDict
 import requests as requests
 from common import OurDB, config  # before `flask`
 import common
-from flask import request, jsonify
+from flask import Response, request, jsonify
 
 app = common.app
 
 
+def serialize_http_request(status_code, url, body, headers) -> bytes:
+    headers_joined = itertools.accumulate(headers.items().map(lambda h: h[0]+"\t"+h[1]+"\r"), operator.add)
+    return (str(status_code) + "\n" + url + "\n" + headers_joined + "\n").encode('utf-8') + body
+
+
+def deserialize_http_request(data: bytes):
+    status_code, url, headers_data, body = data.split("\n", 3)
+    headers = CIMultiDict()
+    for header_data in headers_data.split("\n"):
+        k, v = header_data.split("\t", 1)
+        headers.add(k.decode('utf-8'), v.decode('utf-8'))
+    return int(status_code), url, body, headers
+
+
 # Following https://gist.github.com/questjay/3f858c2fea1731d29ea20cd5cb444e30#file-flask-server-proxy
 def serve_proxied(upstream_path):
-    request_headers = dict(request.headers)
+    request_headers = copy.copy(request.headers)
+    request_body = request.get_data()
+    url = config['upstreamPrefix'] + upstream_path if 'upstreamPrefix' in config else \
+        "https://" + request_headers['host'] + '/' + upstream_path
     filter_request_headers(request_headers)
-    r = make_request(config['upstreamPrefix'] + upstream_path, request.method, params={'key': config['upstreamKey']},
-                     headers=request_headers, data=request.get_data())
-    response_headers = dict(r.raw.headers)
-    filter_response_headers(response_headers)
+    cur_time = int(round(time.time() * 1000))
+    threshold = cur_time - config['cacheTime']
+    with OurDB() as our_db:
+        with our_db.env.begin(our_db.content_db, write=True) as txn:  # TODO: buffers=True allowed?
+            # Remove all outdated entries:
+            cursor = txn.cursor()
+            for key, _value in cursor:
+                key_value = int.from_bytes(key, byteorder='little')
+                if key_value < threshold:
+                    cursor.delete()
 
-    def generate():
-        for chunk in r.iter_content(chunk_size=1024):
-            yield chunk
+            request_data = serialize_http_request(request_body, request_headers)
+            request_data_hasher = hashlib.sha256()
+            request_data_hasher.update(request_data)
+            request_hash = request_data_hasher.digest()
 
-    out = app.response_class(generate(), headers=response_headers)
-    out.status_code = r.status_code
-    return out  # (r.text, r.status_code, headers)
+            old_data = txn.get(request_hash)
+    if old_data is not None:
+        response = {
+            'status': old_data[0],
+            'url': old_data[1],
+            'headers': old_data[2],
+            'body': old_data[3],
+        }
+    else:
+        r = make_request(url, request.method, headers=request_headers, data=request_body)
+        response = {
+            'status': r.status_code,
+            'url': url,
+            'headers': copy.copy(r.raw.headers),
+            'body': r.content,
+        }
+    filter_response_headers(response['headers'])
+
+    return Response(
+        response=[response['body']],
+        status=response['status'],
+        headers=response['headers'],
+        direct_passthrough=True)
+
 
 
 def filter_request_headers(headers):
-    entries_to_remove = [k for k in headers.keys() if k.lower() in ['host', 'x-account-id']]
+    entries_to_remove = [k for k in headers.keys() if k.lower() in ['host']]
     for k in entries_to_remove:
         del headers[k]
     for k, v in config['upstreamHeaders'].items():
@@ -56,7 +105,7 @@ def make_request(url, method, headers={}, data=None, params=None):
     try:
         # LOG.debug("Sending %s %s with headers: %s and data %s", method, url, headers, data)
         print(f"Making request to {url}")
-        return requests.request(method, url, params=params, stream=True,
+        return requests.request(method, url, params=params, stream=False,
                                 headers=headers,
                                 allow_redirects=False,
                                 data=data)
@@ -65,34 +114,8 @@ def make_request(url, method, headers={}, data=None, params=None):
 
 
 @app.route('<path:p>', methods=['GET', 'POST', 'HEAD'])
-def proxy_handler(account, p):
-    account = account.encode('utf-8')
-    for k, v in config['costs'].items():
-        if p.startswith(k):
-            with OurDB() as our_db:
-                with our_db.env.begin(our_db.accounts_db, write=True) as txn:  # TODO: buffers=True allowed?
-                    remainder = txn.get(account)
-                    if remainder is None:
-                        remainder = 0.0
-                    else:
-                        remainder = struct.unpack('<f', remainder)[0]  # float
-                    if v <= remainder:
-                        txn.put(account, struct.pack('<f', remainder - v))
-            if v <= remainder:
-                upstream_path = re.sub(r"^/proxy/[^/]+/", "", request.full_path)
-                return serve_proxied(upstream_path)
-            else:
-                return {
-                    "error_message": "You need to pay for the service.",
-                    "html_attributions": [],
-                    # Hack: Have both `result` and `results` to be sure:
-                    "result": {},
-                    "results": [],
-                    "status": "PAYMENT_REQUIRED"
-                }, 402  # payment required
-
-            break
-    return "Path not found.", 404
+def proxy_handler(p):
+    return serve_proxied(request, p)
 
 
 if __name__ == '__main__':
--- src/common.py
+++ src/common.py
@@ -1,5 +1,4 @@
 import json
-import struct
 
 from flask import Flask
 from flask_compress import Compress
@@ -11,7 +10,7 @@ with open("config.json") as config_file:
 
 
 app = Flask(__name__)
-# Compress(app)
+Compress(app)
 
 
 class OurDB:
@@ -20,39 +19,13 @@ class OurDB:
         self.lock = Lock(f"{config['statePath']}/mylock")
         self.lock.lock(timeout=4)  # FIXME: Ensure, it is not unlocked in the middle.
 
-        self.env = lmdb.open(config['statePath'], max_dbs=10, map_size=200*1024*1024*1024)
-        self.accounts_db = self.env.open_db(b'accounts', create=True)
+        self.env = lmdb.open(
+            config['statePath'],
+            max_dbs=8, map_size=1024*1024*1024*1024)  # terabyte (On 64-bit there is no penalty for making this huge (say 1TB))
+        self.content_db = self.env.open_db(b'content', create=True)
 
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.env.close()
         self.lock.unlock()
-
-    # TODO: Reimplement in Rust, because other backend parts may also require to check this.
-    # Format of records:
-    # (float balance,) - paid
-    # (float balance,int8 end) - trial period ends at `end` seconds since UTC epoch
-    # @staticmethod
-    # def accounts_pack(info):
-    #     if info[1] is not None:
-    #         return struct.pack('<fq', info)
-    #     else:
-    #         return struct.pack('<f', info[0])
-    #
-    # @staticmethod
-    # def accounts_unpack(data):
-    #     try:
-    #         balance, = struct.unpack('<f', data)
-    #         return balance, None
-    #     except struct.error:
-    #         return struct.unpack('<fq', data)
-
-def fund_account(our_db, account, amount):
-    with our_db.env.begin(our_db.accounts_db, write=True) as txn:  # TODO: buffers=True allowed?
-        remainder = txn.get(account)
-        if remainder is None:
-            remainder = 0.0
-        else:
-            remainder = struct.unpack('<f', remainder)[0]  # float
-        txn.put(account, struct.pack('<f', remainder + amount))
