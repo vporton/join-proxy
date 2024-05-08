186bff8c7193b88b9d47d91f83ae435b0e90fcda
Victor Porton <porton.victor@gmail.com>
2024-05-08 05:14:15 +0300

    command line argument

--- src/common.py
+++ src/common.py
@@ -1,11 +1,12 @@
 import json
+import sys
 
 from flask import Flask
 from flask_compress import Compress
 import lmdb
 from flufl.lock import Lock
 
-with open("config.json") as config_file:
+with open(sys.argv[1] or "config.json") as config_file:
     config = json.load(config_file)
 
 
